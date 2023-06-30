5f900e8acf330cc87713630de5b90e562c714ada
Jackson Sheppard <jackson.sheppard@gmail.com>
2023-06-30 16:55:21 -0700

    code to chunk sequences following interleved format

--- phylo/phylo.py
+++ phylo/phylo.py
@@ -97,7 +97,9 @@ for id in species:
         aligned_seqs[new_id] = aligned_seqs.pop(id)
 
 # Check for unique species names for PHYLIP file
-SPECIES_NAME_LEN = 9
+SPECIES_NAME_LEN = 10
+AA_CHUNK_LEN = 10
+N_COLS = 5
 species = list(aligned_seqs.keys())
 phyl_ids = []
 for id in species:
@@ -115,16 +117,20 @@ for id in species:
     new_id = id_spl[-1][:SPECIES_NAME_LEN]
     aligned_seqs[new_id] = aligned_seqs.pop(id)
 
-# Write aligned sequences in PHYLIP format - Using Sequential
-# 5 42 
-# Turkey                  AAGCTNGGGCATTTCAGGGTGAGCCCGGGCAATACAGGGTAT 
-# Salmo_schiefermuelleri  AAGCCTTGGCAGTGCAGGGTGAGCCGTGGCCGGGCACGGTAT 
-# H_sapiens               ACCGGTTGGCCGTTCAGGGTACAGGTTGGCCGTTCAGGGTAA 
-# Chimp                   AAACCCTTGCCGTTACGCTTAAACCGAGGCCGGGACACTCAT 
-# Gorilla                 AAACCCTTGCCGGTACGCTTAAACCATTGCCGGTACGCTTAA
-# 5 = # of species, 42 = length of aligned sequences
-# species has 10 character limit - use first 10 characters (unique)
-
+# Write aligned sequences in PHYLIP format - Using Interleaved format
+#  150 1269
+# Species209   NNNNNNNNNN NNNNNNNNNN NNNNNNNNNN NNNNNNNNNN NNNNNNNNCA
+# Species159   UAUCUGGUUG AUCCUGCCAG UAGUAUNUGC UUGUCUCAAA GAUUAAGCCA
+# ...
+#              UGCAUGUCAG UAUAGCUUUA GUGAAACUGC GAAUGGCUCA UUAAAUCAGU
+#              UGCAUGUCAG UAUAGCUUUA GUGAAACUGC GAAUGGCUNN UUAAAUCAGU
+# ...
+#              ACCUNNNNNN NNNNNNNNN
+#              NNNNNNNNNN NNNNNNNNN
+# ...
+# 150 = # of species, 1269 = length of aligned sequences
+# 10 character species name - use first 10 characters of accession number
+# Sep species and sequence with 3 spaces "   "
 outfile = os.path.join("TreeBASE", "MAPPHY.phy")
 with open(outfile, "w") as f:
     f.write(" " + str(len(aligned_seqs)))
@@ -135,4 +141,12 @@ with open(outfile, "w") as f:
             f.write(id + (SPECIES_NAME_LEN-len(id))*" ")
         else:
             f.write(id)
-        f.write(aligned_seqs[id] + "\n")
+        # write taxon-sequence delim
+        f.write("   ")# + aligned_seqs[id] + "\n")
+        # Write sequence in AA_CHUNK_LEN-element chunks
+        for i in range(0, len(aligned_seqs[id]), AA_CHUNK_LEN):
+            if i % (N_COLS * AA_CHUNK_LEN) == 0:
+                f.write("\n")
+            f.write(aligned_seqs[id][i:i+AA_CHUNK_LEN])
+            f.write(" ")
+        f.write("\n")
